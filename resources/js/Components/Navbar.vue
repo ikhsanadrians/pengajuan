72dc9bb3ad226d2e0373a64266dc07d61f56748b
ikhsanadrians <aikhsan032@gmail.com>
2024-09-23 10:14:36 +0700

    update: navbar

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -4,10 +4,15 @@ import Avatar from 'primevue/avatar';
 
 </script>
 <template>
-    <div class="bg-white h-14 shadow-sm border-b-[1.5px] border-gray-300">
+    <div class="bg-white h-16 shadow-sm border-b-[1.5px] border-gray-300">
         <div class="container mx-auto flex w-full items-center h-full">
-            <div class="w-full flex justify-between">
-                <Link class="flex items-center font-bold" href="/">Pengajuan Barang</Link>
+            <div class="w-full flex justify-between py-8">
+                <Link class="flex items-center font-bold" href="/">
+                  <div class="flex flex-col">
+                    <h1>PT Intertama Trikencana Bersinar</h1>
+                    <p class="text-gray-600 font-light">Pengajuan Barang</p>
+                  </div>
+                </Link>
                 <Avatar label="P" class="mr-2 !bg-red-500" size="xlarge" />
             </div>
 
