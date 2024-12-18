eca0d7c02606e109593356945657456d5359e733
ikhsanadrians <aikhsan032@gmail.com>
2024-12-18 23:25:01 +0700

    feat: filtering user

--- resources/js/Pages/Admin/AddUser.vue
+++ resources/js/Pages/Admin/AddUser.vue
@@ -4,12 +4,12 @@ import Card from 'primevue/card';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Select from 'primevue/select';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import AddNewUserModal from './Components/AddNewUserModal.vue';
 import { useToast } from 'primevue/usetoast';
 import Toast from 'primevue/toast';
 import ConfirmationDeleteUser from './Components/ConfirmationDeleteUser.vue';
-import { router } from '@inertiajs/vue3'; 
+import { router } from '@inertiajs/vue3';
 import UserEditModal from './Components/UserEditModal.vue';
 
 const props = defineProps({
@@ -34,19 +34,26 @@ const loadToastMessage = (toastSeverity, toastSummary, toastMessageDetail) => {
 };
 
 const selectedRoleUser = ref(null);
+const filterName = ref("");
 
-const currentUserToDeleteId = ref(null);
-
-const modalVisibilityAddNewUser = ref(false);
-const modalVisibilityConfirmationDeleteUser = ref(false);
+const filteredUsers = computed(() => {
+    return props.users.filter(user => {
+        const matchesName = user.username.toLowerCase().includes(filterName.value.toLowerCase());
+        const matchesRole = selectedRoleUser.value ? user.role === selectedRoleUser.value.namarole : true;
+        return matchesName && matchesRole;
+    });
+});
 
 const toggleModalAddUser = () => {
     modalVisibilityAddNewUser.value = true;
 };
 
+const modalVisibilityAddNewUser = ref(false);
 const modalVisibilityEditUser = ref(false);
-const currentUserId = ref(null);
+const modalVisibilityConfirmationDeleteUser = ref(false);
 
+const currentUserId = ref(null);
+const currentUserToDeleteId = ref(null);
 
 const toggleEditUserModal = (userId) => {
     console.log(userId);
@@ -63,13 +70,14 @@ const refreshUsers = () => {
     router.reload({ only: ['users'] });
 };
 </script>
+    
 
 <template>
     <Navbar />
     <AddNewUserModal :toastMessage="loadToastMessage" :currentVisibility="modalVisibilityAddNewUser" :rolesUserOption="rolesUser" :departementsUser="departementsUser"/>
     <UserEditModal :toastMessage="loadToastMessage" 
-    :rolesUserOption="rolesUser" :departementsUser="departementsUser" :currentUserId="currentUserId" @update:currentUserId="currentUserId = $event"  :currentVisibility="modalVisibilityEditUser" @update:currentVisibility="modalVisibilityEditUser = $event"
-    @refreshUsers="refreshUsers"/>
+        :rolesUserOption="rolesUser" :departementsUser="departementsUser" :currentUserId="currentUserId" @update:currentUserId="currentUserId = $event"  :currentVisibility="modalVisibilityEditUser" @update:currentVisibility="modalVisibilityEditUser = $event"
+        @refreshUsers="refreshUsers"/>
     <ConfirmationDeleteUser 
         :currentVisibility="modalVisibilityConfirmationDeleteUser"
         @update:currentVisibility="modalVisibilityConfirmationDeleteUser = $event" 
@@ -88,7 +96,7 @@ const refreshUsers = () => {
             </div>
             <div class="wrapper flex gap-x-8 w-full">
                 <div class="wrapper w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-4">
-                    <Card v-for="(user, index) in users" :key="user.id" style="overflow: hidden"
+                    <Card v-for="(user, index) in filteredUsers" :key="user.id" style="overflow: hidden"
                         class="w-full border-[1.8px] border-slate-200 flex flex-col items-center relative">
                         
                         <template #header>
@@ -103,7 +111,7 @@ const refreshUsers = () => {
                             <p class="text-[15px] text-center text-gray-400">{{ user.namalengkap ||  "-" }}</p>
                         </template>
                         <template #content>
-                            <div class="roles  flex justify-center self-center">
+                            <div class="roles flex justify-center self-center">
                                 <p class="text-sm text-gray-500 text-center flex items-center gap-x-1 bg-yellow-200 text-yellow-600 rounded-2xl w-fit px-4 py-1">
                                     <i class="pi pi-id-card"></i> 
                                     {{ user.role.toUpperCase() }}
@@ -125,7 +133,7 @@ const refreshUsers = () => {
                     <div class="input-filter mt-4">
                         <div class="inputs">
                             <label for="" class="text-slate-400">Nama User</label>
-                            <InputText placeholder="Masukan Nama User" class="w-full mt-1" />
+                            <InputText v-model="filterName" placeholder="Masukan Nama User" class="w-full mt-1" />
                         </div>
                         <div class="inputs-select-role mt-4">
                             <label for="" class="text-slate-400">Role User</label>
@@ -133,14 +141,11 @@ const refreshUsers = () => {
                                 placeholder="Pilih Role User" class="w-full mt-1"/>
                         </div>
                         <div class="button-search">
-                            <Button icon="pi pi-search" label="Cari User" class="w-full mt-8">
-
-                            </Button>
+                            <Button icon="pi pi-search" label="Cari User" class="w-full mt-8" />
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
     <Toast position="bottom-right" group="br" />
